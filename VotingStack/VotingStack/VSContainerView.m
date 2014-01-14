f3d1813a8155d99f2c973ce6c253439339e8e646
Yong <Yong@yongs-mbp.att.net>
2014-01-13 22:07:52 -0800

    basic CATransmormations

--- VotingStack/VotingStack/VSContainerView.m
+++ VotingStack/VotingStack/VSContainerView.m
@@ -8,6 +8,22 @@
 
 #import "VSContainerView.h"
 
+
+
+#pragma mark - Macros
+#define RADIANS(deg)  ((deg) * M_PI / 180)
+#define DEGREES(rad)  ((rad) * 180 / M_PI)
+
+
+
+@interface VSContainerView ()
+
+@property (nonatomic, weak) UIView *contenterView;
+
+
+@end
+
+
 @implementation VSContainerView
 
 - (id)initWithFrame:(CGRect)frame
@@ -19,13 +35,55 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+
+
+#pragma mark - View layout
+
+
+- (void) layoutSubviews
 {
-    // Drawing code
+    [super layoutSubviews];
+    self.contenterView.bounds = self.bounds;
+    [self layOutItemViews];
 }
-*/
+
+- (void) layOutItemViews
+{
+    UIView *contentView = [[UIView alloc] init];
+    [@[@(0), @(1), @(2)] enumerateObjectsUsingBlock:^(NSNumber *obj, NSUInteger idx, BOOL *stop) {
+        [contentView addSubview:[self layoutItemView:idx]];
+    }];
+    [self addSubview:contentView];
+    self.contenterView = contentView;
+}
+
+- (UIView *) layoutItemView:(NSUInteger)idx
+{
+    UIView * view = [[UIView alloc] init];
+    view.frame = CGRectMake(10.0f, 10.0f , 100.0f, 150.0f);
+    
+    CATransform3D identity = CATransform3DIdentity;
+    identity.m34 = 1.0/ -1000;
+    CATransform3D rotated = CATransform3DRotate(identity, idx * RADIANS(20), 1, 0, 0);
+    CATransform3D translateAndRoate = CATransform3DTranslate(rotated, idx * 100, idx * 30, idx * 10);
+    
+    view.layer.transform = translateAndRoate;
+    
+    view.backgroundColor = [UIColor redColor];
+    return view;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
