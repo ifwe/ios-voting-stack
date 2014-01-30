fa212ebc453b8d9d31aeb06794725559cfb65390
Yong Xin Dai <ydai@dhcp-office-115.tagged.com>
2014-01-30 15:03:03 -0800

    added library as a target

---
+++ iosVotingStack/iosVotingStackLibrary/iosVotingStackLibrary.m
@@ -0,0 +1,13 @@
+//
+//  iosVotingStackLibrary.m
+//  iosVotingStackLibrary
+//
+//  Created by Yong Xin Dai on 1/30/14.
+//  Copyright (c) 2014 Tagged. All rights reserved.
+//
+
+#import "iosVotingStackLibrary.h"
+
+@implementation iosVotingStackLibrary
+
+@end
