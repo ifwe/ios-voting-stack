f053c9f1d32e04496292d43de1f6b04fa5d98aa1
Yong Xin Dai <ydai@dhcp-office-115.tagged.com>
2014-01-31 16:24:01 -0800

    updated project

---
+++ createStaticLibrary/iosVotingStackTests/iosVotingStackTests.m
@@ -0,0 +1,34 @@
+//
+//  iosVotingStackTests.m
+//  iosVotingStackTests
+//
+//  Created by Yong Xin Dai on 1/31/14.
+//  Copyright (c) 2014 Tagged. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iosVotingStackTests : XCTestCase
+
+@end
+
+@implementation iosVotingStackTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
